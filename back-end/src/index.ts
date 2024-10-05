d5529bec14dcc60003a95da81ad80383c385b736
LucasSilvaa0 <lucaspernambucano@live.com>
2024-10-05 18:28:33 -0300

    Mudanças no back-end

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,143 +1,159 @@
-import express from 'express';
-import { UserModel, MarketProductModel, UpdatePriceModel, CartProductModel } from "./models";
-import { showProducts, offerProduct, delProduct, searchUser, updateProductPrice, newCartProduct, delCartProduct, resetCart, finishShopping } from './functions';
+import express from "express";
+
+import {
+	UserModel,
+	MarketProductModel,
+	UpdatePriceModel,
+	CartProductModel,
+} from "./models";
+import {
+	showProducts,
+	offerProduct,
+	delProduct,
+	searchUser,
+	updateProductPrice,
+	newCartProduct,
+	delCartProduct,
+	resetCart,
+	finishShopping,
+} from "./functions";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
+
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+	console.log(`Servidor rodando na porta ${port}`);
 });
 
+app.post("/", (req, res) => {
+	console.log("Alguém entrou no servidor! ");
 
-app.post('/', (req, res) => {
-    console.log("Alguém entrou no servidor! ")
+	const corpo: JSON = req.body;
 
-    let corpo:JSON = req.body
-
-    res.send(corpo)
+	res.send(corpo);
 });
 
-app.post('/new_user', (req, res) => {
-    console.log("Alguém está tentanto criar um novo usuário! ")
+app.post("/new_user", (req, res) => {
+	console.log("Alguém está tentanto criar um novo usuário! ");
 
-    const newUserScheme = req.body
-    console.log(newUserScheme)
+	const newUserScheme = req.body;
 
-    const result = UserModel.safeParse(newUserScheme);
-    if (result.success) {
-        console.log("Validação bem-sucedida:", result.data);
-        searchUser(newUserScheme, res, 0)
-    } else {
-        console.log("Erro de validação:", result.error.errors);
-    }
-    
+	const result = UserModel.safeParse(newUserScheme);
+	if (result.success) {
+		console.log("Validação bem-sucedida:", result.data);
+		searchUser(newUserScheme, res, 0);
+	} else {
+		console.log("Erro de validação:", result.error.errors);
+	}
 });
 
-app.post('/forgot_password', (req, res) => {
-    console.log("Alguém está querendo lembrar da senha. ")
+app.post("/forgot_password", (req, res) => {
+	console.log("Alguém está querendo lembrar da senha. ");
 
-    const userEmail = req.body
+	const userEmail = req.body;
 
-    searchUser(userEmail, res, 1)
+	searchUser(userEmail, res, 1);
 });
 
-app.post('/login', (req, res) => {
-    console.log("Alguém está querendo logar a conta.")
+app.post("/login", (req, res) => {
+	console.log("Alguém está querendo logar a conta.");
 
-    const user = req.body
+	const user = req.body;
 
-    searchUser(user, res, 2)
+	searchUser(user, res, 2);
 });
 
-app.post('/market/new_product', (req, res) => {
-    console.log("Tem alguém querendo oferecer um novo produto.")
+app.post("/market/new_product", (req, res) => {
+	console.log("Tem alguém querendo oferecer um novo produto.");
 
-    const newProductScheme = req.body
+	const newProductScheme = req.body;
 
-    const result = MarketProductModel.safeParse(newProductScheme)
+	const result = MarketProductModel.safeParse(newProductScheme);
 
-    console.log(result)
+	console.log(result);
 
-    if (result.success) {
-        offerProduct(newProductScheme, res)
-    } else {
-        res.send("Ocorreu algum erro na estrutura do JSON.")
-    }
-})
+	if (result.success) {
+		offerProduct(newProductScheme, res);
+	} else {
+		res.send("Ocorreu algum erro na estrutura do JSON.");
+	}
+});
 
-app.delete(`/market/del_product/:id`, (req, res) => {
-    console.log("Alguém está querendo deletar um produto do mercado.")
+app.delete("/market/del_product/:id", (req, res) => {
+	console.log("Alguém está querendo deletar um produto do mercado.");
 
-    const productId = req.params.id
+	const productId = req.params.id;
 
-    delProduct(productId, res)
-})
+	delProduct(productId, res);
+});
 
-app.put('/market/update_product', (req, res) => {
-    console.log("Alguém está querendo modificar o preço de um produto.")
+app.put("/market/update_product", (req, res) => {
+	console.log("Alguém está querendo modificar o preço de um produto.");
 
-    const updatePriceScheme = req.body
+	const updatePriceScheme = req.body;
 
-    const result = UpdatePriceModel.safeParse(updatePriceScheme)
+	const result = UpdatePriceModel.safeParse(updatePriceScheme);
 
-    if (result.success) {
-        updateProductPrice(updatePriceScheme, res)
-    } else {
-        res.send("Ocorreu algum erro na estrutura do JSON.")
-    }
-})
+	if (result.success) {
+		updateProductPrice(updatePriceScheme, res);
+	} else {
+		res.send("Ocorreu algum erro na estrutura do JSON.");
+	}
+});
 
-app.get('/market/products', (req, res) => {
-    console.log("Alguém está querendo dar uma olhada na loja.")
+app.get("/market/products", (req, res) => {
+	console.log("Alguém está querendo dar uma olhada na loja.");
 
-    showProducts(res, 0)
-})
+	showProducts(res, 0);
+});
 
-app.get(`/market/myproducts/:userId`, (req, res) => {
-    console.log("Alguém está querendo ver seus próprios produtos.")
+app.get("/market/myproducts/:userId", (req, res) => {
+	console.log("Alguém está querendo ver seus próprios produtos.");
 
-    const userId = req.params.userId
+	const userId = req.params.userId;
 
-    showProducts(res, 1, userId)
-})
+	showProducts(res, 1, userId);
+});
 
-app.post('/market/cart/new_product', (req, res) => {
-    console.log("Alguém está querendo adicionar um produto ao carrinho.")
+app.post("/market/cart/new_product", (req, res) => {
+	console.log("Alguém está querendo adicionar um produto ao carrinho.");
 
-    const newProductScheme = req.body
+	const newProductScheme = req.body;
 
-    const result = CartProductModel.safeParse(newProductScheme)
+	const result = CartProductModel.safeParse(newProductScheme);
 
-    if (result.success) {
-        newCartProduct(newProductScheme, res)
-    } else {
-        console.log("Ocorreu algum erro na estrutura do JSON.")
-    }
-})
+	if (result.success) {
+		newCartProduct(newProductScheme, res);
+	} else {
+		console.log("Ocorreu algum erro na estrutura do JSON.");
+	}
+});
 
-app.delete('/market/cart/del_product/:id', (req, res) => {
-    console.log("Alguém está querendo retirar um produto do carrinho.")
+app.delete("/market/cart/del_product/:id", (req, res) => {
+	console.log("Alguém está querendo retirar um produto do carrinho.");
 
-    const productId = req.params.id
+	const productId = req.params.id;
 
-    delCartProduct(productId, res)
-})
+	delCartProduct(productId, res);
+});
 
-app.delete('/market/cart/reset/:id', (req, res) => {
-    console.log("Alguém está querendo esvaziar o carrinho.")
+app.delete("/market/cart/reset/:id", (req, res) => {
+	console.log("Alguém está querendo esvaziar o carrinho.");
 
-    const userId = req.params.id
+	const userId = req.params.id;
 
-    resetCart(userId, res)
-})
+	resetCart(userId, res);
+});
 
-app.get('/market/cart/finish/:id', (req, res) => {
-    console.log("Alguém está querendo comprar os produtos do carrinho.")
+app.get("/market/cart/finish/:id", (req, res) => {
+	console.log("Alguém está querendo comprar os produtos do carrinho.");
 
-    const userId = req.params.id
+	const userId = req.params.id;
 
-    finishShopping(userId, res)
-})
+	finishShopping(userId, res);
+});
--- back-end/src/models.ts
+++ back-end/src/models.ts
@@ -1,27 +1,26 @@
 import { z } from "zod";
-import { delProduct } from "./functions";
 
 export const UserModel = z.object({
-    username: z.string().min(1).max(100),
-    email: z.string().email().min(1).max(100),
-    userpassword: z.string().min(1).max(100)
+	username: z.string().min(1).max(100),
+	email: z.string().email().min(1).max(100),
+	userpassword: z.string().min(1).max(100),
 });
 
 export const MarketProductModel = z.object({
-    seller_id: z.number().int(),
-    product_name: z.string().min(1),
-    product_description: z.string(),
-    product_price: z.number(),
-    photo_link: z.string().url().max(2048)
+	seller_id: z.number().int(),
+	product_name: z.string().min(1),
+	product_description: z.string(),
+	product_price: z.number(),
+	photo_link: z.string().url().max(2048),
 });
 
 export const UpdatePriceModel = z.object({
-    seller_id: z.number().int(),
-    product_id: z.number().int(),
-    new_price: z.number()
+	seller_id: z.number().int(),
+	product_id: z.number().int(),
+	new_price: z.number(),
 });
 
 export const CartProductModel = z.object({
-    cart_user_id: z.number().int(),
-    product_id: z.number().int()
+	cart_user_id: z.number().int(),
+	product_id: z.number().int(),
 });
